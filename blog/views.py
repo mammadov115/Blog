ab6af239dcabbd4065a1add5ab96af9b975c7d99
Mammadov <mehman.mammadov182@gmail.com>
2023-06-10 16:30:57 +0400

    update

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,9 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+# from django.contrib.postgres.search import SearchVector,SearchQuery,SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
+
 
 def post_list(request,tag_slug=None):
     post_list = Post.published.all()
@@ -113,7 +115,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title','body')).filter(search=query)
+            # search_vector =  SearchVector('body',weight='A') + SearchVector('title',weight='B')
+            # search_query = SearchQuery(query)
+            # results = Post.published.annotate(search=search_vector,rank=SearchRank(search_vector,search_query)).filter(rank__gte=0.2).order_by('-rank')
+            results = Post.published.annotate(similarity=TrigramSimilarity('title',query),).filter(similarity__gt=0.1).order_by('-similarity')
+
 
     context = {
         'form':form,
