cae724a59297168cddec114748a668bae6839297
Mehman Mammadov <mehman.mammadov182@gmail.com>
2025-11-13 21:10:20 +0400

    Refactor URL patterns and import statements

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,15 +1,29 @@
-from django.contrib import admin
-from django.urls import path,include
-from django.contrib.sitemaps.views import sitemap
-from blog.sitemaps  import PostSitemap
+"""
+URL configuration for mysite project.
 
+This file defines the URL patterns for the project, including:
+- Django admin interface
+- Blog app URLs
+- Sitemap generation for search engines
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+
+# Dictionary of sitemaps for search engines
 sitemaps = {
-    'posts':PostSitemap,
+    'posts': PostSitemap,  # Sitemap for blog posts
 }
 
 urlpatterns = [
+    # Admin site URL
     path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls',namespace='blog')),
-    path('sitemap.xml',sitemap,{'sitemaps':sitemaps},name='django.contrib.sitemaps.views.sitemap')
 
+    # Blog app URLs
+    path('blog/', include('blog.urls', namespace='blog')),
+
+    # Sitemap URL for search engines
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
